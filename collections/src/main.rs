b40687bad9804f649127e2dee05e9bae6ce82ad5
sujal-GITHUB <sujal21102004@gmail.com>
2025-10-25 15:36:04 +0530

    done

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -5,9 +5,13 @@ fn main() {
     v.push(2);
     v.push(3);
 
-    let v2 = vec![4, 5, 6];
+    let mut v2 = vec![4, 5, 6];
 
     let does_not_exist = v.get(2);
+
+    for i in &mut v2 {
+        *i += 1;
+    }
 
     println!("Vector: {:?}", v2);
     println!("Does not exist: {:?}", does_not_exist);
