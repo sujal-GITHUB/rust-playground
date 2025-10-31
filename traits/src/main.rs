bf5f6abba47d8f5f00a3c0e09e6735ed7c7a9de6
sujal-GITHUB <sujal21102004@gmail.com>
2025-10-31 09:15:59 +0530

    traits implementation

---
+++ traits/src/main.rs
@@ -0,0 +1,46 @@
+trait Summary {
+    fn summarize(&self) -> String;
+}
+
+struct NewsArticle {
+    headline: String,
+    location: String,
+    author: String,
+    content: String,
+}
+
+struct Tweet {
+    username: String,
+    content: String,
+    reply: bool,
+    retweet: bool,
+}
+
+
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+}
+
+fn news_aggregator(source: impl Summary) {
+    println!("Aggregating news articles...");
+    println!("{}",source.summarize());
+}
+
+fn main() {
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+
+    news_aggregator(tweet);
+}
